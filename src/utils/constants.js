fe7f96ea4a8c3ffa301f00c90abc8ba8783f3ba2
RanjithSatla <ranjithsatla327@gmail.com>
2025-01-06 08:49:10 +0530

    Updated File Structure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const MEDIA_ASSETS_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://th.bing.com/th/id/OIG1.NwEgRhaBdqbm6ZYD57oJ?pid=ImgGn";
