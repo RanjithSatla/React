b4d6c726107c163e7f1b2351edba603c10ffdc80
RanjithSatla <ranjithsatla327@gmail.com>
2025-01-09 15:21:04 +0530

    useEffect hook to fetch data dynamically from Swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,41 @@
 import resObj from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const restaurantsList =
+      // json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants.map(
+      //   (restaurant) => {
+      //     return restaurant;
+      //   }
+      // );
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurants(restaurantsList);
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return (
+      <div className="shimmer-container">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <Shimmer key={index} />
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="body">
       {/* <div className="search"></div> */}
