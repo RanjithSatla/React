abc28ec71dcd4be07cf5299a33d08221888be36f
RanjithSatla <ranjithsatla327@gmail.com>
2025-02-02 17:02:53 +0530

    Linked Home,About,Contact Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  console.log("Header Rendered");
 
+  useEffect(() => {
+    console.log("Use Effect Called");
+  }, []);
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,9 +18,16 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>Aboubt Us</li>
-          <li>Contact Us</li>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us </Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us </Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
