4ca2fd2061cc55d736d9b7d89a01c2f5a5e6ff40
RanjithSatla <ranjithsatla327@gmail.com>
2025-03-12 08:30:52 +0530

    Class based component with two child components

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,37 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
 
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       {/* <User /> */}
+//       <UserClass name={"Ranjith Satla"} location={"Hyderabad"} />
+//     </div>
+//   );
+// };
+
+class About extends React.Component {
+  constructor() {
+    super();
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent ComponentDidMount");
+  }
+
+  render() {
+    console.log("Parent Render");
+
+    return (
+      <div>
+        <h1>About Us</h1>
+        <UserClass name={"First"} location={"Hyderabad"} />
+        <UserClass name={"Second"} location={"Hyderabad"} />
+      </div>
+    );
+  }
+}
 export default About;
