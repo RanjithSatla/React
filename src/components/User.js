e01cced164a79b4981a0bf5518538412a683283a
RanjithSatla <ranjithsatla327@gmail.com>
2025-03-12 08:32:12 +0530

    User Functional component

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="usercard">
+      <h2>Name : Ranjith Satla</h2>
+      <h3>Location : Hyderabad</h3>
+      <h4>Contact : @ranjithsatla327</h4>
+    </div>
+  );
+};
+
+export default User;
