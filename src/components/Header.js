3f8e756e5055a69261d44b036945cf76964e42ee
RanjithSatla <ranjithsatla327@gmail.com>
2025-03-13 08:47:48 +0530

    Show online status on Header using custom Hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
   console.log("Header Rendered");
+
+  const onlineStatus = useOnlineStatus();
+  console.log(onlineStatus);
 
   useEffect(() => {
     console.log("Use Effect Called");
@@ -18,6 +22,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status : {onlineStatus ? "💚" : "💔"}</li>
           <li>
             {" "}
             <Link to="/">Home</Link>
@@ -27,6 +32,9 @@ const Header = () => {
           </li>
           <li>
             <Link to="/contact">Contact Us </Link>
+            <li>
+              <Link to="/grocery">Grocery</Link>
+            </li>
           </li>
           <li>Cart</li>
           <button
