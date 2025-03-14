c0ea4801b7adb9e7d684e6c2331e8217256c21ef
RanjithSatla <ranjithsatla327@gmail.com>
2025-03-14 12:53:19 +0530

    Restaurant List Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,6 @@ export const LOGO_URL =
 
 export const MENU_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.014238846337&lng=73.11294313520193&restaurantId=";
+
+export const RESTAURANT_LIST_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
